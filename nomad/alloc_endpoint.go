276fa834f117e67f92055ba37668bbfa585ec8ee
Allison Larson <allisonlarson91@gmail.com>
2026-04-02 15:15:36 -0700

    allow WI to get extra claims from namespace config

--- nomad/alloc_endpoint.go
+++ nomad/alloc_endpoint.go
@@ -552,6 +552,10 @@ func (a *Alloc) SignIdentities(args *structs.AllocIdentitiesRequest, reply *stru
 		}
 
 		job := out.Job
+		ns, err := a.srv.State().NamespaceByName(nil, out.Namespace)
+		if err != nil {
+			return err
+		}
 
 		switch idReq.WorkloadType {
 		case structs.WorkloadTypeTask:
@@ -565,7 +569,7 @@ func (a *Alloc) SignIdentities(args *structs.AllocIdentitiesRequest, reply *stru
 				continue
 			}
 
-			widFound, err := a.signTasks(task, out, idReq, reply, now)
+			widFound, err := a.signTasks(task, out, ns, idReq, reply, now)
 			if err != nil {
 				return err
 			}
@@ -579,7 +583,7 @@ func (a *Alloc) SignIdentities(args *structs.AllocIdentitiesRequest, reply *stru
 			}
 
 		case structs.WorkloadTypeService:
-			widFound, err := a.signServices(job, out, idReq, reply, now)
+			widFound, err := a.signServices(job, out, ns, idReq, reply, now)
 			if err != nil {
 				return err
 			}
@@ -599,6 +603,7 @@ func (a *Alloc) SignIdentities(args *structs.AllocIdentitiesRequest, reply *stru
 func (a *Alloc) signTasks(
 	task *structs.Task,
 	alloc *structs.Allocation,
+	ns *structs.Namespace,
 	idReq *structs.WorkloadIdentityRequest,
 	reply *structs.AllocIdentitiesResponse,
 	now time.Time,
@@ -609,7 +614,7 @@ func (a *Alloc) signTasks(
 		}
 
 		widFound = true
-		builder := structs.NewIdentityClaimsBuilder(alloc.Job, alloc, &idReq.WIHandle, wid).
+		builder := structs.NewIdentityClaimsBuilder(alloc.Job, alloc, &idReq.WIHandle, wid, ns).
 			WithTask(task).
 			WithConsul()
 
@@ -635,6 +640,7 @@ func (a *Alloc) signTasks(
 func (a *Alloc) signServices(
 	job *structs.Job,
 	alloc *structs.Allocation,
+	ns *structs.Namespace,
 	idReq *structs.WorkloadIdentityRequest,
 	reply *structs.AllocIdentitiesResponse,
 	now time.Time,
@@ -646,7 +652,7 @@ func (a *Alloc) signServices(
 		for _, service := range tg.Services {
 			if service.IdentityHandle(nil).Equal(wid) {
 				claims := structs.NewIdentityClaimsBuilder(
-					alloc.Job, alloc, &idReq.WIHandle, service.Identity).
+					alloc.Job, alloc, &idReq.WIHandle, service.Identity, ns).
 					WithConsul().
 					WithService(service).
 					Build(now)
@@ -657,7 +663,7 @@ func (a *Alloc) signServices(
 			for _, service := range task.Services {
 				if service.IdentityHandle(nil).Equal(wid) {
 					claims := structs.NewIdentityClaimsBuilder(
-						alloc.Job, alloc, &idReq.WIHandle, service.Identity).
+						alloc.Job, alloc, &idReq.WIHandle, service.Identity, ns).
 						WithTask(task).
 						WithConsul().
 						WithService(service).
